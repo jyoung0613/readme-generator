4c51537e97796db8149a596e82267d8aba844fb6
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-09-19 16:00:16 -0600

    added assets and images folders

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./assets/utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
